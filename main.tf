1fba53db81099047612c532ca8b0e93c2085f2c5
Makarand.Jadhav <makarand.jadhav@aerlingus.com>
2020-06-25 17:42:40 +0100

    Deploying a configurable web server

--- main.tf
+++ main.tf
@@ -5,8 +5,37 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-089cc16f7f08c4457"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, Mak" > index.html
+              nohup busybox httpd -f -p ${var.server_port} &
+              EOF
 
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = var.server_port
+    protocol = "tcp"
+    to_port = var.server_port
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type = number
+  default = 8080
+}
+
+output "public_ip" {
+  value = aws_instance.example.public_ip
+  description = "The public IP address of the web server"
 }
